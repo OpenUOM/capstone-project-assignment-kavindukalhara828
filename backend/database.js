3a150501ff15637837d24706b3af1a8efb16dc5e
kavindukalhara828 <156567385+kavindukalhara828@users.noreply.github.com>
2024-01-29 14:17:08 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -138,7 +138,7 @@ const updateStudent = async (name, age, religion, id) => {
     const sql = `UPDATE student SET name=?, age=?, religion=? WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [name, age, id, religion ])
+            .raw(sql, [name, age, religion, id ])
             .then(() => {
                 resolve({status: "Successfully updated Student"});
             })
